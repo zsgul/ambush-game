28ceea0102479055ded3869cb46cc904c61707e4
Zeynep <zesudegul@gmail.com>
2021-12-09 23:40:40 +0300

    Fireball Behaviours
    Fireball animations, Fireball script, animation events

---
+++ Ambush/Assets/Scripts/Projectile.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private bool hit;
+    private float direction;
+
+    private Animator anim;
+    private BoxCollider2D boxCollider;
+    
+
+    private void Awake() 
+    {
+        anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update() 
+    {
+        if(hit) return;
+
+        float movementSpeed = speed * Time.deltaTime * direction;
+        transform.Translate(movementSpeed, 0, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision) 
+    {
+        hit = true;
+        boxCollider.enabled = false;
+        anim.SetTrigger("explode");
+    }
+
+    public void SetDirection(float _direction) 
+    {
+        direction = _direction;
+        gameObject.SetActive(true);
+        hit = false;
+        boxCollider.enabled = true;
+
+        float localScaleX = transform.localScale.x;
+        if(Mathf.Sign(localScaleX) != _direction)
+        {
+            localScaleX = -localScaleX;
+        }
+
+        transform.localScale = new Vector3(localScaleX, transform.localScale.y, transform.localScale.z);
+    }
+
+    private void Deactivate()
+    {
+        gameObject.SetActive(false);
+    }
+        
+    
+    
+
+}
