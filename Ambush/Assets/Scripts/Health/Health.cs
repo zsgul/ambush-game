ad78000db65a6b50c2f48bc0f9417d22380791a4
Zeynep <zesudegul@gmail.com>
2021-12-20 20:31:53 +0300

    iFrames Tweaking

--- Ambush/Assets/Scripts/Health/Health.cs
+++ Ambush/Assets/Scripts/Health/Health.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [Header ("Health")]
     [SerializeField] private float startingHealth;
     public float currentHealth{get; private set;}
     private Animator anim;
     private bool dead;
 
+    [Header ("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private float numberOfFlashes;
+    private SpriteRenderer spriteRend;
+
     private void Awake() 
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(float _damage) 
@@ -24,6 +31,7 @@ public class Health : MonoBehaviour
         {
             //hurt
             anim.SetTrigger("hurt");
+            StartCoroutine(Invulnerability());
         }
         else
         {
@@ -47,4 +55,18 @@ public class Health : MonoBehaviour
     {
         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
     }
+
+    private IEnumerator Invulnerability()
+    {
+        Physics2D.IgnoreLayerCollision(10, 11, true);
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1,0,0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(10, 11, false);
+    }
 }
+ 
