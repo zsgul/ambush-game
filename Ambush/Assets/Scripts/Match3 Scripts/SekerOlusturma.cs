4e926c6c1ce633e09e53cfb1856467838ca73d12
kubrakrkmzz <58361852+kubrakrkmzz@users.noreply.github.com>
2021-12-23 19:45:48 +0300

    Match3 Selected

--- Ambush/Assets/Scripts/Match3 Scripts/SekerOlusturma.cs
+++ Ambush/Assets/Scripts/Match3 Scripts/SekerOlusturma.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 
 public class SekerOlusturma : MonoBehaviour
 {
-    public GameObject[] urunler;
-    public int row, column;
-    public float x = -7.79f, y = -2.88f;
+       public GameObject[] pre_urunler;
+       public int row, column;
+       public float x = -7.79f, y = -2.88f;
+       public int urunX = 0, urunY = 0;
+       public Urun[,] o_urunler;
+    
 
     void Start()
     {
-        for(int i=0; i < row; i++)
+        o_urunler = new Urun[row, column];
+
+        for (int i = 0; i < row; i++)
         {
-            for (int j=0; j< column; j++)
+            for (int j = 0; j < column; j++)
             {
                 SekerOlustur();
                 x += 2.31f;
+                urunY += 1;
             }
             y += 1.98f;
             x = -7.79f;
+            urunX += 1;
+            urunY = 0;
         }
     }
 
@@ -30,12 +38,15 @@ public class SekerOlusturma : MonoBehaviour
     public void SekerOlustur()
     {
         GameObject yeni_urun = GameObject.Instantiate(RandomUrun(), new Vector2(x, y+10), Quaternion.identity);
-        yeni_urun.GetComponent<Urun>().YeniKonum(x, y);//yavaþ yavaþ düþmesini saðlýyoruz position deðiþikliði
+        Urun urun = yeni_urun.GetComponent<Urun>();//yavaþ yavaþ düþmesini saðlýyoruz position deðiþikliði
+        urun.YeniKonum(x, y);
+        o_urunler[urunX, urunY] = urun; //matris oluþturarak bütün þekerlere istenilen yerden ulaþma
+        //Debug.Log(o_urunler[urunX, urunY]);
     }
 
     public GameObject RandomUrun()
     {
-        int rnd = Random.Range(0, urunler.Length);
-        return urunler[rnd];
+        int rnd = Random.Range(0, pre_urunler.Length);
+        return pre_urunler[rnd];
     }
 }
--- Ambush/Assets/Scripts/Match3 Scripts/Urun.cs
+++ Ambush/Assets/Scripts/Match3 Scripts/Urun.cs
@@ -4,26 +4,28 @@ using UnityEngine;
 
 public class Urun : MonoBehaviour
 {
-    float x, y;
-    bool DussunMu=true;
 
+    float x, y;
+    bool DussunMu = true;
+    GameObject SecimAraci;
+    
     void Start()
     {
-        
+        SecimAraci = GameObject.FindGameObjectWithTag("Secim");
     }
 
+    // Update is called once per frame
     void Update()
     {
         if (DussunMu)
         {
-            /*if (transform.position.y - y < 0.2f)
+            if (transform.position.y - y < 0.05f)
             {
                 DussunMu = false;
                 transform.position = new Vector3(x, y, 0);
-            }*/
+            }
 
-
-            transform.position = Vector3.Lerp(transform.position, new Vector3(x, y, 0), Time.deltaTime * 4f);
+            transform.position = Vector3.Lerp(transform.position, new Vector3(x, y, 0), Time.deltaTime * 3f);
         }
     }
 
@@ -32,5 +34,13 @@ public class Urun : MonoBehaviour
         x = _x;
         y = _y;
 
+ 
     }
+
+    void OnMouseDown()
+    {
+        SecimAraci.transform.position = transform.position;
+    }
+
+
 }
