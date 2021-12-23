1ed86debe6b174e1a96b053a39731600986dd36d
kubrakrkmzz <58361852+kubrakrkmzz@users.noreply.github.com>
2021-12-23 21:46:40 +0300

    Match3 position added

--- Ambush/Assets/Scripts/Match3 Scripts/Urun.cs
+++ Ambush/Assets/Scripts/Match3 Scripts/Urun.cs
@@ -8,13 +8,17 @@ public class Urun : MonoBehaviour
     float x, y;
     bool DussunMu = true;
     GameObject SecimAraci;
+    public static Urun ilkSecilenUrun;
+    public static Urun ikinciSecilenUrun;
+    public Vector3 HedefKonum;
+    public bool yerDegisim = false;
     
     void Start()
     {
         SecimAraci = GameObject.FindGameObjectWithTag("Secim");
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         if (DussunMu)
@@ -26,6 +30,11 @@ public class Urun : MonoBehaviour
             }
 
             transform.position = Vector3.Lerp(transform.position, new Vector3(x, y, 0), Time.deltaTime * 3f);
+        }
+
+        if (yerDegisim)
+        {
+            YerDegis();
         }
     }
 
@@ -40,7 +49,57 @@ public class Urun : MonoBehaviour
     void OnMouseDown()
     {
         SecimAraci.transform.position = transform.position;
+        UrunDurumu();
+
+
     }
 
+    void UrunDurumu()
+    {
+        if (ilkSecilenUrun == null)
+        {
+            ilkSecilenUrun = this;
+        }
+        else
+        {
+            ikinciSecilenUrun = this;
+            if (ilkSecilenUrun != ikinciSecilenUrun)
+            {
+                float fark_x = ilkSecilenUrun.x - ikinciSecilenUrun.x;
+                float fark_y = ilkSecilenUrun.y - ikinciSecilenUrun.y;
+                Debug.Log(fark_x + fark_y);
+
+                if (fark_x + fark_y >= -4.29)
+                {
+                    Debug.Log("Degissinler");
+                    //KONTROL SAÐLANMADI DÝKKAT!!
+                    ilkSecilenUrun.HedefKonum = ikinciSecilenUrun.transform.position; // yer deðiþtirsinler.
+                    ikinciSecilenUrun.HedefKonum = ilkSecilenUrun.transform.position;
+                    ilkSecilenUrun.yerDegisim = true;
+                    ikinciSecilenUrun.yerDegisim = true;
+                    ilkSecilenUrun = null;
+                    ikinciSecilenUrun = null;
+
+
+                }
+
+                else
+                {
+                    ilkSecilenUrun = ikinciSecilenUrun;
+                    ikinciSecilenUrun = null;
+                }
+            }
+            else
+            {
+                ikinciSecilenUrun = null;
+            }
+
+        }
+    }
+
+    void YerDegis()
+    {
+        transform.position = Vector3.Lerp(transform.position, HedefKonum, 0.1f);
+    }
 
 }
